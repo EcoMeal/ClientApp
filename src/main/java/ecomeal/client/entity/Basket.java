9f88ad5a92b9492b4175222a5c2932bfb931dcab
Maxime Opsommer <maxime.opsommer@gmail.com>
2017-12-04 22:51:38 +0100

    successfully connected to the rest API, entities need to be changed for the Schedule (Horaire) page

--- src/main/java/ecomeal/client/entity/Basket.java
+++ src/main/java/ecomeal/client/entity/Basket.java
@@ -5,21 +5,33 @@ import java.util.List;
 
 public class Basket {
 	
-	private int id;
-	private BasketCategory basketCategory;
+	//private int id;
+	//private BasketCategory basketCategory;
 	private String name;
+	private Integer price;
+	private String category;
+	//private String category_image;
 	private List<Product> products;
 	private File image;
 	
-	public Basket(int id, BasketCategory basketCategory, String name, List<Product> products, File image) {
+	/*public Basket(int id, BasketCategory basketCategory, String name, List<Product> products, File image) {
 		this.id = id;
 		this.basketCategory = basketCategory;
 		this.name = name;
 		this.products = products;
 		this.image = image;
+	}*/
+	
+	public Basket(String name, Integer price, String category, String category_image, List<Product> products) {
+		this.name = name;
+		this.price = price;
+		this.category = category;
+		category_image = (category_image.equals(null) || category_image.isEmpty())? "null_logomark_400x400.jpg" : category_image;
+		this.image = new File(getClass().getClassLoader().getResource(category_image).getFile());
+		this.products = products;
 	}
 	
-	public int getId() {
+	/*public int getId() {
 		return id;
 	}
 	
@@ -33,7 +45,7 @@ public class Basket {
 	
 	public void setBasketCategory(BasketCategory basketCategory) {
 		this.basketCategory = basketCategory;
-	}
+	}*/
 	
 	public String getName() {
 		return name;
@@ -42,6 +54,30 @@ public class Basket {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	/*public String getCategory_image() {
+		return category_image;
+	}
+
+	public void setCategory_image(String category_image) {
+		this.category_image = category_image;
+	}*/
 	
 	public List<Product> getProducts() {
 		return products;
--- src/main/java/ecomeal/client/services/BasketService.java
+++ src/main/java/ecomeal/client/services/BasketService.java
@@ -1,17 +1,30 @@
 package ecomeal.client.services;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import ecomeal.client.entity.Basket;
 import ecomeal.client.entity.BasketCategory;
 import ecomeal.client.entity.Product;
 import ecomeal.client.entity.ProductCategory;
 
+@ApplicationScoped
 public class BasketService extends AbstractService {
 	
 	private final static BasketService INSTANCE = new BasketService();
@@ -24,24 +37,52 @@ public class BasketService extends AbstractService {
 	 * @return all available Basket objects.
 	 */
 	public synchronized List<Basket> findAll() {
-		return getFakeBaskets();
+		
+		List<Basket> res = new ArrayList<Basket>();
+		
+		try {
+			URL url = new URL("http://vps434333.ovh.net/api/basket");
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.connect();
+			InputStream inputStream = connection.getInputStream();
+			
+			String result = inputStreamToString(inputStream, 1024);
+			
+			JSONArray array = new JSONArray(result);
+			JSONObject obj;
+			for(int i = 0; i < array.length(); i++) {
+				obj = (JSONObject) array.get(i);
+				Basket basket = new Basket((String) obj.get("name"), (Integer) obj.get("price"), (String) obj.get("category"), "", new ArrayList<Product>());
+				res.add(basket);
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return res;
 	}
 
 	// TODO : Need REST url from web app
 	@Override
 	public void init() {
-		client = ClientBuilder.newClient();
-		target = client.target("url");
+		//client = ClientBuilder.newClient();
+		//target = client.target("http://vps434333.ovh.net/api/basket");
 	}
 	
 	
-	public Product getProductResponse(String place) {
-		return target.queryParam("q", place).request(MediaType.APPLICATION_JSON).get(Product.class);
+	public List<Basket> getProductResponse(String place) {
+		//return target.queryParam("q", place).request(MediaType.APPLICATION_JSON).get(Product.class);
+		List<Basket> res = new ArrayList<Basket>();
+		res.add(target.request(MediaType.APPLICATION_JSON).get(Basket.class));
+		return res;
 	}
 	
 	
 	// Temporary list of basket to test the user interface
-	private List<Basket> getFakeBaskets() {
+	/*private List<Basket> getFakeBaskets() {
 		List<Basket> res = new ArrayList<Basket>();
 		BasketCategory classic = new BasketCategory(1, "Classique");
 		ProductCategory meat = new ProductCategory(1, "Viande");
@@ -75,6 +116,6 @@ public class BasketService extends AbstractService {
 		res.add(new Basket(7, classic, "Classic Chicken 4", classicChicken, chickenFile));
 		res.add(new Basket(8, classic, "Classic Beef 4", classicBeef, beefFile));
 		return res;
-	}
+	}*/
 
 }
