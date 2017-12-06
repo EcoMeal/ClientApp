7bbec20ddbde96905acdec5ab84a3a2efbe94f78
Opsommer Maxime <maxime.opsommer@gmail.com>
2017-12-06 13:59:02 +0100

    add url wrapper

---
+++ src/main/java/ecomeal/client/tools/UrlWrapper.java
@@ -0,0 +1,20 @@
+package ecomeal.client.tools;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class UrlWrapper {
+
+    URL url;
+
+    public UrlWrapper(String spec) throws MalformedURLException{
+        url = new URL(spec);
+    }
+
+    public URLConnection openConnection() throws IOException{
+        return url.openConnection();
+    }
+
+}
