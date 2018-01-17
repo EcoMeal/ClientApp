35d7895bede6e004c51efbbc3b7aa70eb79f92d7
Opsommer Maxime <maxime.opsommer@gmail.com>
2018-01-17 16:10:50 +0100

    on rentre à la maison :D

--- src/main/java/ecomeal/client/tools/postTool.java
+++ src/main/java/ecomeal/client/tools/postTool.java
@@ -6,10 +6,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringWriter;
+import java.net.URISyntaxException;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
@@ -31,65 +33,8 @@ public class postTool {
 	 */
 	public String postMessage(UrlWrapper url, String json){	
 		
-		/*URL serverAddress = url.getUrl();
-        HttpURLConnection connection;
 		try {
-			connection = (HttpURLConnection) serverAddress.openConnection();
-		} catch (IOException e) {
-			e.printStackTrace();
-			return "postMessage : IOException";
-		}
-        connection.setDoOutput(true);
-        connection.setDoInput(true);
-        connection.setRequestProperty("Content-Type", "application/json");
-        connection.setRequestProperty("X-HTTP-Method-Override", "POST");
-        try {
-			connection.setRequestMethod("POST");
-		} catch (ProtocolException e) {
-			e.printStackTrace();
-			return "postMessage : ProtocolException";
-		}
-
-        try {
-        	OutputStream os = connection.getOutputStream();
-        	BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
-        	
-			writer.write(json);
-			writer.flush();
-	        writer.close();
-	        os.close();
-	        connection.connect();
-	        
-	        InputStream _is;
-	        if (connection.getResponseCode() / 100 == 2) { // 2xx code means success
-	            _is = connection.getInputStream();  
-	        } else {  
-
-	            _is = connection.getErrorStream();  
-
-	            String result = inputStreamToString(_is);
-	            System.out.println("Error != 2xx" + result);
-	        }
-	        
-	        
-	        String json_response = "";
-	        InputStreamReader in = new InputStreamReader(_is);
-	        BufferedReader br = new BufferedReader(in);
-	        String text = "";
-	        while ((text = br.readLine()) != null) {
-	          json_response += text;
-	        }
-		    System.out.println("Response : " + json_response);
-	        return json_response;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return "postMessage : IOException";
-		}*/
-		
-		
-		try {
-			
-			
+				
 			StringEntity requestEntity = new StringEntity(json, ContentType.APPLICATION_JSON);
 
 			HttpClient client = HttpClients.createDefault();
@@ -97,16 +42,10 @@ public class postTool {
 			postMethod.setEntity(requestEntity);
 
 			HttpResponse response = client.execute(postMethod);
-			
-	        /*HttpPost request = new HttpPost();
-	        request.setURI(url.getUrl().toURI());
-	        request.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
-	        request.setEntity(new StringEntity(json));
-	        HttpResponse response = client.execute(request);*/
 	
 	        InputStream ips  = response.getEntity().getContent();
 	        BufferedReader buf = new BufferedReader(new InputStreamReader(ips,"UTF-8"));
-	        if(response.getStatusLine().getStatusCode()!=HttpStatus.SC_OK)
+	        if(response.getStatusLine().getStatusCode() != HttpStatus.SC_OK)
 	        {
 	            throw new Exception(response.getStatusLine().getReasonPhrase());
 	        }
@@ -125,7 +64,17 @@ public class postTool {
 	        System.out.println(sb.toString());
 	        return sb.toString();
 	        
-		} catch(Exception e) {
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return json;
