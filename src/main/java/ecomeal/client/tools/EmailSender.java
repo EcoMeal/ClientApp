42d1da1dcf9422c9c8b732527b0058852148d342
Delassus Alexandre <delassus@b10p24.fil.univ-lille1.fr>
2018-01-23 15:51:29 +0100

    first try mail

--- src/main/java/ecomeal/client/tools/EmailSender.java
+++ src/main/java/ecomeal/client/tools/EmailSender.java
@@ -2,46 +2,72 @@ package ecomeal.client.tools;
 
 import java.util.Properties;
 
+import javax.mail.Folder;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.NoSuchProviderException;
 import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import javax.mail.Store;
 
 public class EmailSender {
-	
-	public static void sendEmail(String adresse, String titre, String messageText){
-		
-	     
-	        //ouverture d'une session. la session gére les informations de configuration (nom d'utilisateur, mot de passe, hôte) nécessaires pour utiliser les fonctionnalités de JavaMail
-	        Properties props= new Properties();
-	        props.setProperty("mail.from", "alexandre.d.info@gmail.com"); // @ expediteur
-	        props.put("mail.smtp.auth", "true");
-	        props.put("mail.smtp.starttls.enable", "true");
-	        props.put("mail.smtp.host", "smtp.gmail.com");
-	        props.put("mail.smtp.port", "587");
-	        Session session= Session.getInstance(props);
-	         
-	        //Le message
-	        Message     message     = new MimeMessage(session);
-	        InternetAddress recipient;
-			try {
-				recipient = new InternetAddress(adresse);
-				message.setRecipient(Message.RecipientType.TO, recipient);
-				message.setSubject(titre);
-		        message.setText(messageText);
-		         
-		        //Transport
-		        Transport.send(message);
-			} catch (MessagingException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+
+	public static void main(String[] args) {
+		String host = "pop.gmail.com";// change accordingly
+		String mailStoreType = "pop3";
+		String username = "ecomealsociete@gmail.com";// change accordingly
+		String password = "jambonbeurre";// change accordingly
+
+		check(host, mailStoreType, username, password);
+
+	}
+
+
+	public static void check(String host, String storeType, String user,
+			String password) 
+	{
+		try {
+
+			//create properties field
+			Properties properties = new Properties();
+
+			properties.put("mail.pop3.host", host);
+			properties.put("mail.pop3.port", "995");
+			properties.put("mail.pop3.starttls.enable", "true");
+			Session emailSession = Session.getDefaultInstance(properties);
+
+			//create the POP3 store object and connect with the pop server
+			Store store = emailSession.getStore("pop3");
+
+			store.connect(host, user, password);
+
+			//create the folder object and open it
+			Folder emailFolder = store.getFolder("INBOX");
+			emailFolder.open(Folder.READ_ONLY);
+
+			// retrieve the messages from the folder in an array and print it
+			Message[] messages = emailFolder.getMessages();
+			System.out.println("messages.length---" + messages.length);
+
+			for (int i = 0, n = messages.length; i < n; i++) {
+				Message message = messages[i];
+				System.out.println("---------------------------------");
+				System.out.println("Email Number " + (i + 1));
+				System.out.println("Subject: " + message.getSubject());
+				System.out.println("From: " + message.getFrom()[0]);
+				System.out.println("Text: " + message.getContent().toString());
+
 			}
-	        
-	        
-		
+
+			//close the store and folder objects
+			emailFolder.close(false);
+			store.close();
+
+		} catch (NoSuchProviderException e) {
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
